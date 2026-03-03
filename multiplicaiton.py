7b9a27138445d93b382a0342b7936868424339bb
periko81 <periko@byu.edu>
2026-03-03 10:11:09 -0700

    This file multiplies two numbers together

---
+++ multiplicaiton.py
@@ -0,0 +1,8 @@
+# Author: Erik Petesen
+# Section 003
+#3/3/26
+# Description: This program takes two numbers and multiplies them together.
+# It then prints the result.
+def multiplyTwoNumbers(iNum1,iNum2):
+    return iNum1 * iNum2
+
