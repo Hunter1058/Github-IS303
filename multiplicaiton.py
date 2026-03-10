b271a7d2e059827259ecf106c1c17312700875a2
periko81 <periko@byu.edu>
2026-03-10 09:44:56 -0600

    This is the opening display

--- multiplicaiton.py
+++ multiplicaiton.py
@@ -4,5 +4,5 @@
 # Description: This program takes two numbers and multiplies them together.
 # It then prints the result.
 def multiplyTwoNumbers(iNum1,iNum2):
+    #multiply and return the result
     return iNum1 * iNum2
-
